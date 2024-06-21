dd727ef195c400f64055e9cdee217b793776a648
Bruno Pavlović <bpavlovic20@foi.hr>
2024-06-21 02:00:04 +0200

    Documented endpoints
    Implemented SwaggerConfiguration class with details about server and contact. Documented ProductController endpoints with @Operation, @Parameters, and @ApiResponse annotations for completeness of API documentation.

---
+++ src/main/java/com/example/middleware/config/SwaggerConfiguration.java
@@ -0,0 +1,32 @@
+package com.example.middleware.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfiguration {
+
+        @Bean
+        public OpenAPI defineOpenApi() {
+            Server server = new Server();
+            server.setUrl("http://localhost:8080");
+            server.setDescription("Development");
+
+            Contact myContact = new Contact();
+            myContact.setName("Bruno Pavlovic");
+            myContact.setEmail("bruno.pavlovic66@gmail.com");
+
+            Info information = new Info()
+                    .title("Product Middleware API")
+                    .version("1.0")
+                    .description("This API exposes endpoints that are working with data from dummyjson.com .")
+                    .contact(myContact);
+            return new OpenAPI().info(information).servers(List.of(server));
+        }
+}
--- src/main/java/com/example/middleware/controllers/ProductController.java
+++ src/main/java/com/example/middleware/controllers/ProductController.java
@@ -2,6 +2,13 @@ package com.example.middleware.controllers;
 
 import com.example.middleware.model.Product;
 import com.example.middleware.services.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +26,13 @@ public class ProductController {
         this.productService = productService;
     }
 
+    @Operation(summary = "Get all products",
+            description = "Get all products from API. The response is list of Products or error message.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = Product.class)) }),
+            @ApiResponse(responseCode = "500", description = "No products found!",
+                    content = @Content) })
     @GetMapping
     public ResponseEntity<?> getAllProducts() {
         List<Product> products =  productService.getAllProducts();
@@ -28,6 +42,15 @@ public class ProductController {
         return new ResponseEntity<>("No products found!", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    @Operation(summary = "Get product by id",
+            description = "Get products with given id from API. The response is a Product or error message.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = Product.class)) }),
+            @ApiResponse(responseCode = "404", description = "No product found with given id!",
+                    content = @Content) })
+    @Parameter( description = "ID of product to be retrieved",
+            required = true)
     @GetMapping("/{id}")
     public ResponseEntity<?> getProductById(@PathVariable int id) {
         Product product = productService.getProductById(id);
@@ -38,6 +61,19 @@ public class ProductController {
         return new ResponseEntity<>("Product with id " + id + " not found!", HttpStatus.NOT_FOUND);
     }
 
+    @Operation(summary = "Get products by filter",
+            description = "Get products that matches applied filter. Products can be filtered by category and price" +
+                    ". The response is list of Products or error message.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = Product.class)) }),
+            @ApiResponse(responseCode = "404", description = "No products found with given filter!",
+                    content = @Content) })
+    @Parameters({
+            @Parameter(name = "category", description = "Category of the product", required = false, schema = @Schema(type = "String")),
+            @Parameter(name = "minPrice", description = "Minimum price of the product", required = false, schema = @Schema(type = "integer")),
+            @Parameter(name = "maxPrice", description = "Maximum price of the product", required = false, schema = @Schema(type = "integer"))
+    })
     @GetMapping("/filter")
     public ResponseEntity<?> filterProducts(@RequestParam(required = false) String category,
                                             @RequestParam(required = false) Double minPrice,
@@ -47,9 +83,17 @@ public class ProductController {
             return new ResponseEntity<>(products, HttpStatus.OK);
         }
 
-        return new ResponseEntity<>("No products found with the given filter!", HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>("No products found with given filter!", HttpStatus.NOT_FOUND);
     }
 
+    @Operation(summary = "Get products by search",
+            description = "Get all products from API that matches title." +
+                    " The response is list of Products or error message.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = Product.class)) }),
+            @ApiResponse(responseCode = "404", description = "No products found with given title!",
+                    content = @Content) })
     @GetMapping("/search")
     public ResponseEntity<?> searchProducts(@RequestParam String title){
         List<Product> products = productService.searchProducts(title);
